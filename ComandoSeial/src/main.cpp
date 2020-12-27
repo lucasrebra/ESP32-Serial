20ad3a3410b4072121ed705ef21aac5e8ada63aa
lucas <lucasrebra97@gmail.com>
2020-12-27 21:20:42 +0100

    Comandos Hola Mundo por serial del ordenador

---
+++ ComandoSeial/src/main.cpp
@@ -0,0 +1,78 @@
+#include <Arduino.h>
+#include <SerialCommand.h>
+#include <BluetoothSerial.h>
+
+
+SerialCommand sCmd;     // The demo SerialCommand object
+void LED_on() {
+  Serial.println("LED on");
+}
+
+void LED_off() {
+  Serial.println("LED off");
+}
+
+void sayHello() {
+  char *arg;
+  arg = sCmd.next();    // Get the next argument from the SerialCommand object buffer
+  if (arg != NULL) {    // As long as it existed, take it
+    Serial.print("Hello I'm ESP32");
+    Serial.println(arg);
+  }
+  else {
+    Serial.println("Hello, I'M ESP32 !!!!!!!!");
+  }
+}
+
+
+void processCommand() {
+  int aNumber;
+  char *arg;
+
+  Serial.println("We're in processCommand");
+  arg = sCmd.next();
+  if (arg != NULL) {
+    aNumber = atoi(arg);    // Converts a char string to an integer
+    Serial.print("First argument was: ");
+    Serial.println(aNumber);
+  }
+  else {
+    Serial.println("No arguments");
+  }
+
+  arg = sCmd.next();
+  if (arg != NULL) {
+    aNumber = atol(arg);
+    Serial.print("Second argument was: ");
+    Serial.println(aNumber);
+  }
+  else {
+    Serial.println("No second argument");
+  }
+}
+
+// This gets set as the default handler, and gets called when no other command matches.
+void unrecognized(const char *command) {
+  Serial.println("What?");
+}
+
+void setup() {   // Configure the onboard LED for output
+
+  Serial.begin(115200);
+
+  // Setup callbacks for SerialCommand commands
+  sCmd.addCommand("ON",    LED_on);          // Turns LED on
+  sCmd.addCommand("OFF",   LED_off);         // Turns LED off
+  sCmd.addCommand("HELLO", sayHello);        // Echos the string argument back
+  sCmd.addCommand("P",     processCommand);  // Converts two arguments to integers and echos them back
+  sCmd.setDefaultHandler(unrecognized);      // Handler for command that isn't matched  (says "What?")
+  Serial.println("Ready");
+}
+
+
+void loop() {
+  sCmd.readSerial();     // We don't do much, just process serial commands
+  
+}
+
+
